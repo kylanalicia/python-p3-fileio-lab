6fcb963448e6382cdebebd4eba462d76d4036970
kylanalicia <alicia.kimani@student.moringaschool.com>
2023-08-22 12:31:43 +0300

    completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,21 @@
 def write_file(file_name, file_content):
-    pass
+    try:
+        with open(file_name + ".txt", "w") as file:
+            file.write(file_content)
+    except FileNotFoundError:
+        return "File not found"
 
-def append_file(file_name, append_content):
-    pass
+def append_to_file(file_name, append_content):
+    try:
+        with open(file_name + ".txt", "a") as file:
+            file.write(append_content)
+    except FileNotFoundError:
+        return "File not found"
 
 def read_file(file_name):
-    pass
+    try:
+        with open(file_name + ".txt", "r") as file:
+            content = file.read()
+            return content
+    except FileNotFoundError:
+        return "File not found"
